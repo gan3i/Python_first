d205dd62d638e01b29590a2d2ea9ab5ee3d47c80
Ganesha Nageshappa <gnageshappa@aptean.com>
2018-10-08 20:16:07 +0530

    fetching list of strings

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,1 +1,33 @@
-print("Hello!..World")
+"""print("Hello!..World")"""
+# import math
+# math.sqrt(81)
+# from math import factorial as fact
+# n=13
+# r=3
+# print(fact(n)/(fact(r)*fact(n-r)))
+# print(fact(20))
+# for i in range(5):
+#     x=i*10
+#     print(x)
+from urllib.request import urlopen
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+	story_words=[]
+	for line in story:
+		line_words=line.split()
+		for word in line_words:
+			story_words.append(word)
+
+
+print(story_words)
+
+            
+
+
+
+            
+
+
+
+
+
+
