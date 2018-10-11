059e9ac22258837565f35c7f7a8c080df20e1092
Ganesha Nageshappa <gnageshappa@aptean.com>
2018-10-11 20:09:28 +0530

    Collections completed

---
+++ collection_2.py
@@ -0,0 +1,10 @@
+"""SET"""
+#mutable collection of unordered elements . sets are iterable, and membership operaters works on set
+#p={6,3,55,34,3,8,23232}
+# print(p)
+# print(type(p))
+# e=set()#empty set
+# s=set([2,3,3,4,2,234,43,8])#from list- duplicates are discarded
+# #s.add(233)
+#p.update([23,23,23,2,2,2,2])
+print("g")
